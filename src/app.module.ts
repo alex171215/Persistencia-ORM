3ffb541c19aa58e9430f0f240183b757fadec45f
alex171215 <alexandercarrion918@gmail.com>
2026-09-16 11:30:11 -0500

    fix: resolver extensiones ESM en tests y restaurar AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductosModule } from './productos/productos.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
@@ -22,5 +25,7 @@ import { ProductosModule } from './productos/productos.module';
     }),
     ProductosModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
